995543b36bcf66dfde8cd20c6d8b20973d402398
Siva Rama Chakradhar Ramisetti <146643092+SivaRamaChakradhar@users.noreply.github.com>
2024-03-04 20:33:18 +0530

    import java.util.*; public class Main {     public static void main(String[] args)     {         Scanner s = new Scanner(System.in);         int i, j, n;         n = s.nextInt();         for(i=1;i<=n;i++)         {             for(j=1;j<=n;j++)             {                 System.out.print("*"+" ");             }             System.out.println();         }     } }

---
+++ Basic_Pattern_1.1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner s = new Scanner(System.in);
+        int i, j, n;
+        n = s.nextInt();
+        for(i=1;i<=n;i++)
+        {
+            for(j=1;j<=n;j++)
+            {
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+    }
+}
