3e62ab8d8ef29b83bd93a394111fc23f08db4497
Siva Rama Chakradhar Ramisetti <146643092+SivaRamaChakradhar@users.noreply.github.com>
2024-03-20 19:42:18 +0530

    import java.util.*; public class Main{     public static void main(String[] args)     {         Scanner scan = new Scanner(System.in);         int t = scan.nextInt();         int s = scan.nextInt();         int b = scan.nextInt();         System.out.println(t*b*s+" KB");     } }

---
+++ Capacity.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        int s = scan.nextInt();
+        int b = scan.nextInt();
+        System.out.println(t*b*s+" KB");
+    }
+}
