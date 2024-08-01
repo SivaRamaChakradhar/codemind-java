dcdab1b33382934775b59cecedac5878c37a88df
Siva Rama Chakradhar Ramisetti <146643092+SivaRamaChakradhar@users.noreply.github.com>
2024-08-01 11:23:40 +0530

    import java.util.*; public class main{     public static void main(String[] args)     {         Scanner s = new Scanner(System.in);         int x = s.nextInt();         int y = s.nextInt();         int z = s.nextInt();         int p = 2*(x+y);         System.out.println(p*z);     } }

---
+++ Frames.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class main{
+    public static void main(String[] args)
+    {
+        Scanner s = new Scanner(System.in);
+        int x = s.nextInt();
+        int y = s.nextInt();
+        int z = s.nextInt();
+        int p = 2*(x+y);
+        System.out.println(p*z);
+    }
+}
